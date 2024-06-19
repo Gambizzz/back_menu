bd81fabe7570c0c031bdc700c0739d68984336bc
Gambizzz <anelisegambini@yahoo.fr>
2024-06-19 19:53:05 +0200

    index + update + edit restau OK

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_admin!, except: [:index, :show]
+  before_action :authorize_admin!, only: [:update, :destroy]
 
   def index
     if params[:city].present? && params[:food].present?
@@ -31,12 +32,34 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      render json: @restaurant, status: :ok
+    else
+      render json: { errors: @restaurant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    render json: { message: "Restaurant deleted successfully" }, status: :ok
+  end
+
   private
+
+  def authorize_admin!
+    @restaurant = Restaurant.find(params[:id])
+    unless @restaurant.admin_id == current_admin.id
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :description, :admin_id, :city, :food)
   end
 end
+
 
 
 
