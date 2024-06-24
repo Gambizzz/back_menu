f2c58b69c3754e83f25c2861a1b6a6e712a20d46
NicolasBaldo <septfoisept@outlook.fr>
2024-06-24 12:14:12 +0200

    Réservation phase 4

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ApplicationRecord
-    after_create :reservation_send
-    before_destroy :cancel_send
+
   
     belongs_to :restaurant
     belongs_to :user
@@ -10,13 +9,5 @@ class Reservation < ApplicationRecord
     validates :date, presence: true
     validates :time, presence: true
   
-    def reservation_send
-      UserMailer.reservation_email(self).deliver_now
-      AdminMailer.reservation_email(self).deliver_now
-    end
-  
-    def cancel_send
-      UserMailer.cancel_email(self.user, self).deliver_now
-      AdminMailer.cancel_email(self.restaurant.admin.email, self).deliver_now
-    end
+   
   end
